6c0cc8a93fcbae5cb3ed868c052a38fddac61a25
Clark Gunn <clark.gunn@formidable.com>
2025-02-24 16:43:31 -0500

    waitlist init

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+
+const ReactCompilerConfig = { target: '19' };
+
+export default defineConfig({
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.tsx'],
+      ssr: 'resources/js/ssr.jsx',
+      refresh: true,
+    }),
+    react({ babel: { plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]] } }),
+    tailwindcss(),
+  ],
+  esbuild: {
+    jsx: 'automatic',
+  },
+});
